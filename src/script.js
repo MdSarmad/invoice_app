fcdd6b0853b32f335b6a85138778bb1d208babab
MdSarmad <28mdsarmad99@gmail.com>
2021-11-08 22:25:13 +0530

    js implemented

--- src/script.js
+++ src/script.js
@@ -6,4 +6,37 @@
 // const pin = document.getElementById('pin').value;
 // // const note
 
-const addItem = document.getElementsByClassName('add-item-button');
+const addItem = document.querySelector(".add-item-button");
+addItem.addEventListener("click",newItem);
+var body = document.querySelector('.invoice-container-right-body');
+
+function newItem(e){
+    e.preventDefault();
+    
+    var row = `
+    <div class="invoice-container-right-body-row">
+        <div class="item-no-input">
+            2
+        </div>
+        <div class="item-name-input">
+            <input type="text" name="item-input-name" id="item-input-name" class="item-input-name">
+        </div>
+        <div class="item-price-input">
+            <input type="number" name="item-input-price" id="item-input-price" class="item-input-price">   
+        </div>
+        <div class="item-quantity-input">
+            <input type="number" name="item-input-quantity" id="item-input-quantity" class="item-input-quantity">   
+        </div>
+        <div class="item-amount-input">
+            <input type="number" name="tem-input-amount" id="tem-input-amount" class="item-input-amount">   
+        </div>
+        <div class="add-item-input">
+            <button class="add-item-button"><h1>+</h1></button>
+        </div>
+    </div>`
+
+    body.appendChild(row);
+    console.log("clicked");
+
+                    
+}
