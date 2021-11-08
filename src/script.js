12aa0e454ec8faeb7cac2fe97500da7d2c6344c1
MdSarmad <28mdsarmad99@gmail.com>
2021-11-08 13:26:42 +0530

    updated ui commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// const fName = document.getElementById('fname').value;
+// const lName = document.getElementById('lname').value;
+// const eMail = document.getElementById('email').value;
+// const mob = document.getElementById('mob').value;
+// const addr = document.getElementById('address').value;
+// const pin = document.getElementById('pin').value;
+// // const note
+
+const addItem = document.getElementsByClassName('add-item-button');
